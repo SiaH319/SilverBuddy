a2f8805fa1cb974c71265202fd85de5a6f5600a3
1dudals2 <Deau3141>
2021-09-11 17:44:23 -0400

    dada

--- lib/signupPage.dart
+++ lib/signupPage.dart
@@ -138,7 +138,7 @@ class _signupPageState extends State<signupPage> {
                     FirebaseFirestore.instance
                         .collection('users')
                         .add({'email': email, 'username': name, 'uid': uid});
-                    pageModel.changePageId(4);
+                    pageModel.changePageId(4);zzz
                   });
                 },
                 child: Container(
